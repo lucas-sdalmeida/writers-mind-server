c71d1b002bd0e3a2fa9b4fe0df678e9faeceeb4a
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-06-02 20:18:59 -0300

    feat: improve TimeLine class

--- src/main/kotlin/com/lucassdalmeida/writing/domain/model/timeline/TimeLine.kt
+++ src/main/kotlin/com/lucassdalmeida/writing/domain/model/timeline/TimeLine.kt
@@ -3,31 +3,29 @@ package com.lucassdalmeida.writing.domain.model.timeline
 import com.lucassdalmeida.writing.domain.model.fragment.ExcerptId
 import com.lucassdalmeida.writing.shared.Entity
 import java.time.LocalDateTime
+import java.util.TreeMap
+import kotlin.math.abs
 
 class TimeLine(id: TimeLineId) : Entity<TimeLineId>(id) {
-    private val lines = mutableListOf<MutableMap<Double, TimeMark>>()
+    private val _lines = mutableListOf<MutableMap<Double, TimePoint>>()
+    val lines get() = _lines
+        .map { it.values.toList() }
 
-    fun addMark(mark: TimeMark) {
-        for (line in lines) {
-            if (mark.position in line) continue
-            line[mark.position] = mark
+    fun addPoint(point: TimePoint) {
+        for (line in _lines) {
+            if (line.keys.hasValueNear(point.pointX)) continue
+            line[point.pointX] = point
             return
         }
-        lines.add(mutableMapOf(mark.position to mark))
+        _lines.add(TreeMap(mutableMapOf(point.pointX to point)))
     }
 
-    fun removeMarkByExcerptId(excerptId: ExcerptId) {
-        lines.forEach { line ->
-            val mark = line.entries
-                .find { (_, value) -> value.excerptId == excerptId }
-            mark?.let { line.remove(it.key) }
-        }
-    }
+    private fun Set<Double>.hasValueNear(pointX: Double) = any { abs(it - pointX) <= 1 }
 
-    data class TimeMark(
-        val excerptId: ExcerptId,
-        val position: Double,
-        val description: String?,
+    data class TimePoint(
+        val fragmentId: ExcerptId,
+        val title: String,
+        val pointX: Double,
         val moment: LocalDateTime?,
     )
 }
