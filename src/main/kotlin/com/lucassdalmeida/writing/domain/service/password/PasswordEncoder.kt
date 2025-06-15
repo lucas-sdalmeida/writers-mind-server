507e01012539ce3353b6e248f35b280d021d6406
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-06-15 17:18:30 -0300

    refactor: renamed password encoder class

--- src/main/kotlin/com/lucassdalmeida/writing/domain/service/password/PasswordEncoder.kt
+++ src/main/kotlin/com/lucassdalmeida/writing/domain/service/password/PasswordEncoder.kt
@@ -1,7 +1,7 @@
 package com.lucassdalmeida.writing.domain.service.password
 
-interface PasswordEncrypter {
-    fun encrypt(password: String): String
+interface PasswordEncoder {
+    fun encode(password: String): String
 
     fun check(plain: String, encrypted: String)
 }
