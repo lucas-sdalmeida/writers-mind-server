d63b3f696ccb939110c751101cd2c3714ee2c009
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-06-17 20:49:17 -0300

    feat: delete time line related classes from application

--- src/main/kotlin/com/lucassdalmeida/writing/application/timeline/repository/TimeLineRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.lucassdalmeida.writing.application.timeline.repository
-
-import java.util.UUID
-
-interface TimeLineRepository {
-    fun save(dto: TimeLineDto)
-
-    fun findById(id: UUID): TimeLineDto?
-}
