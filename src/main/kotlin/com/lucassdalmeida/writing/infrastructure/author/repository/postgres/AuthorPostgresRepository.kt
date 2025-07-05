2bc2a34a770d9cf921d5adc1b9e1fef1cfddfd9e
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2025-07-05 16:26:06 -0300

    feat: implement author repository method for finding by account id

--- src/main/kotlin/com/lucassdalmeida/writing/infrastructure/author/repository/postgres/AuthorPostgresRepository.kt
+++ src/main/kotlin/com/lucassdalmeida/writing/infrastructure/author/repository/postgres/AuthorPostgresRepository.kt
@@ -1,8 +1,11 @@
 package com.lucassdalmeida.writing.infrastructure.author.repository.postgres
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 import java.util.UUID
 
 interface AuthorPostgresRepository : JpaRepository<AuthorDataModel, UUID> {
+    fun findByAccountId(accountId: UUID): Optional<AuthorDataModel>
+
     fun existsByPseudonym(pseudonym: String): Boolean
 }
--- src/main/kotlin/com/lucassdalmeida/writing/infrastructure/author/repository/postgres/AuthorRepositoryImpl.kt
+++ src/main/kotlin/com/lucassdalmeida/writing/infrastructure/author/repository/postgres/AuthorRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.lucassdalmeida.writing.application.author.repository.AuthorDto
 import com.lucassdalmeida.writing.application.author.repository.AuthorRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
+import kotlin.jvm.optionals.getOrNull
 
 @Repository
 class AuthorRepositoryImpl(
@@ -12,6 +13,11 @@ class AuthorRepositoryImpl(
     override fun save(dto: AuthorDto) {
         innerRepository.save(dto.toDataModel())
     }
+
+    override fun findByAccountId(accountId: UUID) = innerRepository
+        .findByAccountId(accountId)
+        .getOrNull()
+        ?.toDto()
 
     override fun existsById(id: UUID) = innerRepository.existsById(id)
 
